e775fd30bf899c5e8295b359d6bb07f60611d81e
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-12-07 23:40:47 +0530

    functions

---
+++ js/function.js
@@ -0,0 +1,29 @@
+function sum(a,b) {
+    console.log(a+b)
+}
+
+function multi(a,b) {
+    console.log(a*b)
+}
+
+sum(5,7)
+multi(7,8)
+
+// arrow function 
+const arrowsum=(a,b)=>{
+    console.log(a+b);
+}
+
+
+// Q1 create a function using the "function" keyword that takes a strings as an argunment & return the numbers of vowels in the strings 
+function countvowel(str){
+    let count = 0;
+    for (const char  of str) {
+        if (char=== "a" || char=== "e" || char === "i" || char === "o" || char === "u") {
+            count++
+        }
+    }
+    console.log(`your entered string (${str}) have ${count} vowels`)
+}
+
+countvowel("dhanraj")
