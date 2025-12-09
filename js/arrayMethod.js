d30b1e23b30f88865a0c21669c015c68e6da6572
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-12-09 16:58:12 +0530

    array methods

--- js/arrayMethod.js
+++ js/arrayMethod.js
@@ -35,3 +35,47 @@ companies.splice(1, 1, "ola");
 // add amazon at the end
 companies.push("amazon");
 console.log(companies);
+
+// for-each loop in arrays
+//foreach("CallBackFunction")  it is a function to execute for each element in the array
+// callBack function pased as an agrunmnet to another function
+
+let arr = [2, 3, 4, 5, 6];
+arr.forEach((val) => {
+  console.log(val * 2);
+});
+
+//Q2 for a given array of number, print the square of each value using the foreach loop
+console.log("question 2");
+let num = [2, 4, 6, 8];
+num.forEach((num) => {
+  console.log(num * num);
+});
+
+//Q3 we are given array of marks of student. filter out the marks of  student that scored 90+
+console.log("Question 3");
+let marks = [89, 78, 60, 90, 99, 98, 100];
+let toppers = marks.filter((val) => {
+  return val > 90;
+});
+console.log(toppers.toString());
+
+//Q4Take a number n as input from user. Create an array of numbers from 1 to n.
+//Use the reduce method to calculate sum of all numbers in the array.
+// Use the reduce method to calculate product of all numbers in the array.
+console.log("Question 4");
+let n = prompt("enter a number between 1 to 10");
+let numbArr = [];
+for (let i = 1; i <= n; i++) {
+  numbArr[i - 1] = i;
+}
+console.log(numbArr);
+
+let sumArr = numbArr.reduce((result, current) => {
+  return result * current;
+});
+console.log(sumArr);
+
+let fact = numbArr.reduce((result, current) => {
+  return result * current;
+});
