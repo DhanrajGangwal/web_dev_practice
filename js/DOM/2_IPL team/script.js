d3cd14e46eb0ec18f327bccc8f6892fb62918dbd
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2026-02-10 00:21:59 +0530

    IPL team

---
+++ js/DOM/2_IPL team/script.js
@@ -0,0 +1,46 @@
+let btn = document.querySelector('button');
+let main = document.querySelector('main');
+let team = [
+    {
+        team: 'CSK',
+        primary: 'yellow',
+        secondary: 'blue',
+    },
+    {
+        team: 'RCB',
+        primary: 'red',
+        secondary: 'black'
+    },
+    {
+        team: 'MI',
+        primary: 'blue',
+        secondary: 'gold'
+    },
+    {
+        team: 'KKR',
+        primary: 'purple',
+        secondary: 'gold'
+    },
+    {
+        team: 'SRH',
+        primary: 'orange',
+        secondary: 'black'
+    },
+    {
+        team: 'PBKS',
+        primary: 'crimson',
+        secondary: 'silver'
+    },
+]
+
+btn.addEventListener('click',()=>{
+    let winner = team[Math.floor(Math.random()*team.length)]
+    // var winner = arr[Math.floor(Math.random()*arr.length)]
+    let c1 = Math.floor(Math.random())*256
+    let c2 = Math.floor(Math.random())*256
+    let c3 = Math.floor(Math.random())*256
+    btn.innerHTML = winner.team;
+    btn.style.backgroundColor = winner.primary;
+    btn.style.color = "gray";
+    main.style.backgroundColor = winner.secondary;
+})
