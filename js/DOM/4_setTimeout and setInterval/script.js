730bf7ff47b09204aed89b87f3b8113007209701
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2026-02-10 22:57:23 +0530

    setTimeout, setInterval, clearInterval

---
+++ js/DOM/4_setTimeout and setInterval/script.js
@@ -0,0 +1,18 @@
+let bar = document.querySelector('#bar');
+var btn = document.querySelector('button');
+let h1 = document.querySelector('h1');
+var grow = 0;
+btn.addEventListener('click',()=>{
+    let time = setInterval(() => {
+        grow++;
+        bar.style.width = grow + '%';
+        h1.innerHTML = `${grow}%`;
+        if(grow === 100){
+            h1.innerHTML = "dowload complete"
+        }
+
+    }, 100);
+    setTimeout(() => {
+        clearInterval(time);
+    }, 10000); 
+});
